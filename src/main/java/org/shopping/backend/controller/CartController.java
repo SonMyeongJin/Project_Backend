1d89c87487a026b785c8febef1af1de4adccdd22
SonMyeongJin <smj2802@naver.com>
2024-04-15 22:53:22 +0900

    add cart page

---
+++ src/main/java/org/shopping/backend/controller/CartController.java
@@ -0,0 +1,74 @@
+package org.shopping.backend.controller;
+
+import org.shopping.backend.entity.Cart;
+import org.shopping.backend.entity.Item;
+import org.shopping.backend.repository.CartRepository;
+import org.shopping.backend.repository.ItemRepository;
+import org.shopping.backend.service.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.List;
+
+@RestController
+public class CartController {
+
+    @Autowired
+    JwtService jwtService;
+
+    @Autowired
+    CartRepository cartRepository;
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    @GetMapping("/api/cart/items")
+    public ResponseEntity getCartItems(@CookieValue(value = "toeken", required = false) String token) {
+
+        if (!jwtService.isValid(token)) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+        }
+
+        int memberId = jwtService.getId(token);
+        List<Cart> carts = cartRepository.findByMemberId(memberId);
+        List<Integer> itemIds = carts.stream().map(Cart::getItemId).toList();
+        List<Item> items = itemRepository.finByIdIn(itemIds);
+
+        return new ResponseEntity<>(carts, HttpStatus.OK);
+    }
+
+
+    @PostMapping("/api/cart/items/{itemId}")
+    public ResponseEntity pushCartItem(
+            @PathVariable("itemId") int itemId,
+            @CookieValue(value = "token", required = false) String token
+    ) {
+
+        if (!jwtService.isValid(token)) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+        }
+
+        int memberId = jwtService.getId(token);
+        Cart cart = cartRepository.findByMemberIdAndItemId(memberId, itemId);
+
+        if (cart == null) {
+            Cart newCart = new Cart();
+            newCart.setMemberId(memberId);
+            newCart.setItemId(itemId);
+            cartRepository.save(newCart);
+        }
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+}
--- src/main/java/org/shopping/backend/repository/ItemRepository.java
+++ src/main/java/org/shopping/backend/repository/ItemRepository.java
@@ -3,6 +3,9 @@ package org.shopping.backend.repository;
 import org.shopping.backend.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item, Integer> {
 
+    List<Item> finByIdIn(List<Integer> ids);
 }
